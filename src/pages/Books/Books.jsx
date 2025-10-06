58a978a59c26348b418834e1862f1fd8941d78b1
iamalshahed <shahedevise@gmail.com>
2025-10-07 00:32:29 +0600

    load and display books data from api

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import SingleBook from "../SingleBook/SingleBook";
 
-const Books = () => {
+const Books = ({ booksData }) => {
   return (
-    <div>Books</div>
-  )
-}
+    <div className="my-20">
+      {/* title */}
+      <div className="">
+        <h1 className="text-neutral-900 text-4xl font-bold mb-9 text-center">
+          Books
+        </h1>
+      </div>
 
-export default Books
+      {/* books card */}
+      <Suspense fallback={<span>loading...</span>}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {booksData.map((bookItem, index) => (
+            <SingleBook bookItem={bookItem} key={index} />
+          ))}
+        </div>
+      </Suspense>
+    </div>
+  );
+};
+
+export default Books;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import Books from "../Books/Books";
+import { useLoaderData } from "react-router";
 
 const Home = () => {
+  const booksData = useLoaderData();
+
   return (
     <div>
       <Banner />
+      <Books booksData={booksData} />
     </div>
   );
 };
